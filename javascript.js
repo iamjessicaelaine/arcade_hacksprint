6489a842760338f6fb98003a6c78198fb2bbfdbe
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-08-18 10:16:51 -0700

    functional quiz:

--- javascript.js
+++ javascript.js
@@ -44,8 +44,7 @@ name="riddle${riddleNumber}" value=${letter}">
     // find selected answer
       const answerContainer = answerContainers[riddleNumber];
       const selector = `input[name=riddle${riddleNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) ||
-{}).value;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
       // if answer is correc
       if (userAnswer === currentRiddle.correctAnswer) {
       // add to the number of correct answers
@@ -69,8 +68,25 @@ name="riddle${riddleNumber}" value=${letter}">
     slides[n].classList.add('active-slide');
     currentSlide = n;
     if (currentSlide === 0) {
-      submitButton.style.display = 'inline-block';
+      previousButton.style.display = 'none';
+    } else {
+      previousButton.style.display = 'inline-block';
     }
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    } else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide () {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide () {
+    showSlide(currentSlide - 1);
   }
 
   // variables
@@ -111,6 +127,8 @@ name="riddle${riddleNumber}" value=${letter}">
   buildQuiz();
 
   // pagination
+  const previousButton = document.getElementById('previous');
+  const nextButton = document.getElementById('next');
   const slides = document.querySelectorAll('.slide');
   let currentSlide = 0;
 
@@ -119,4 +137,6 @@ name="riddle${riddleNumber}" value=${letter}">
 
   // on submit, show results
   submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener('click', showPreviousSlide);
+  nextButton.addEventListener('click', showNextSlide);
 })();
